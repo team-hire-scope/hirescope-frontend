88a60c6a1e5aa1380ffb16378b4144594eaa22f9
SeongHwan <epaktldk0203@gmail.com>
2026-04-17 00:53:38 +0900

    feat: 페이지 이동 시 스크롤 상단 위치하도록 개선

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,9 +1,10 @@
-import { Outlet } from 'react-router'
+import { Outlet, ScrollRestoration } from 'react-router'
 import { TopNav } from './TopNav'
 
 export const Layout = () => {
 	return (
 		<div className='min-h-screen w-full bg-white text-black'>
+			<ScrollRestoration />
 			<div className='flex min-h-screen w-full flex-col'>
 				<TopNav />
 				<main className='flex-1 w-full'>
